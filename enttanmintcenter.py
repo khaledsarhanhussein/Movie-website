abe6677f968c7fb2b20926bba5d4a216c323a39f
khaled sarhan <engk.masry@gmail.com>
2018-03-09 03:05:20 +0200

    3nd

---
+++ enttanmintcenter.py
@@ -0,0 +1,7 @@
+import media
+toy_story=media.Movie("toy story",
+ 	"astory of a boy " ,
+ 	"https://vignette.wikia.nocookie.net/disney/images/8/80/Toy_Story_-_Poster.jpg/revision/latest?cb=20150108180742",
+ 	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+print toy_story.storyline
+print toy_story.trailer_youtube_url
